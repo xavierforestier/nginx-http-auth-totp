1183e40778558c99882338761fe44d315dc9d76f
xavierforestier <23277701+xavierforestier@users.noreply.github.com>
2026-03-01 18:21:58 +0100

    Update test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -2,12 +2,12 @@ SECRET=ZK2OTLT4AYP3TBYHYLJ45CGYD5RKD7HS
 
 echo -en " Test 1: \033[1mModule do nothing without auth_totp_realm directive\033[0m... "
 http_code=$( curl -sio /tmp/last -w "%{http_code}" http://localhost:8080/public/index.html )
-[ "${http_code}" == "404" ] || echo -e "❌ 033[1;31mFAILED\033[0m (expected 404, but got ${http_code})"
-[ "${http_code}" == "404" ] && echo -e "✅ \033[1;32mPASSED\033[0m"
+[ "${http_code}" == "200" ] || echo -e "❌ 033[1;31mFAILED\033[0m (expected 200, but got ${http_code})"
+[ "${http_code}" == "200" ] && echo -e "✅ \033[1;32mPASSED\033[0m"
 echo "::group::Response from nginx"
 cat /tmp/last
 echo "::endgroup::" 
-[ "${http_code}" == "404" ] || exit 1
+[ "${http_code}" == "200" ] || exit 1
 
 echo -en " Test 2: \033[1mauth_totp_realm 'something' block access\033[0m... "
 http_code=$( curl -sio /tmp/last -w "%{http_code}" http://localhost:8080/priv6d30s/index.html )
@@ -29,30 +29,30 @@ echo "::endgroup::"
 
 echo -en " Test 4: \033[1mauth_totp_realm off allow access of a subfolder\033[0m... "
 http_code=$( curl -sio /tmp/last -w "%{http_code}" http://localhost:8080/priv6d30s/public/index.html )
-[ "${http_code}" == "404" ] || echo -e "❌ \033[1;31mFAILED\033[0m (expected 404, but got ${http_code})"
-[ "${http_code}" == "404" ] && echo -e "✅ \033[1;32mPASSED\033[0m"
+[ "${http_code}" == "200" ] || echo -e "❌ \033[1;31mFAILED\033[0m (expected 200, but got ${http_code})"
+[ "${http_code}" == "200" ] && echo -e "✅ \033[1;32mPASSED\033[0m"
 echo "::group::Response from nginx"
 cat /tmp/last
 echo "::endgroup::" 
-[ "${http_code}" == "404" ] || exit 4
+[ "${http_code}" == "200" ] || exit 4
 
 echo -en " Test 5: \033[1mTest a valid standard token (6 digits / 30sec)\033[0m... "
 http_code=$( curl -u:admin:$( python tests/getOTP.py ${SECRET} ) -sio /tmp/last -w "%{http_code}" http://localhost:8080/priv6d30s/index.html )
-[ "${http_code}" == "404" ] || echo -e "❌ \033[1;31mFAILED\033[0m (expected 404, but got ${http_code})"
-[ "${http_code}" == "404" ] && echo -e "✅ \033[1;32mPASSED\033[0m"
+[ "${http_code}" == "200" ] || echo -e "❌ \033[1;31mFAILED\033[0m (expected 200, but got ${http_code})"
+[ "${http_code}" == "200" ] && echo -e "✅ \033[1;32mPASSED\033[0m"
 echo "::group::Response from nginx"
 cat /tmp/last
 echo "::endgroup::" 
-[ "${http_code}" == "404" ] || exit 5
+[ "${http_code}" == "200" ] || exit 5
 
 echo -en " Test 6: \033[1mTest a valid non-standard token (8 digits / 60sec)\033[0m... "
 http_code=$( curl -u:admin:$( python tests/getOTP.py ${SECRET} -d 60 -l 8 ) -sio /tmp/last -w "%{http_code}" http://localhost:8080/priv8d60s/index.html )
-[ "${http_code}" == "404" ] || echo -e "❌ \033[1;31mFAILED\033[0m (expected 404, but got ${http_code})"
-[ "${http_code}" == "404" ] && echo -e "✅ \033[1;32mPASSED\033[0m"
+[ "${http_code}" == "200" ] || echo -e "❌ \033[1;31mFAILED\033[0m (expected 200, but got ${http_code})"
+[ "${http_code}" == "200" ] && echo -e "✅ \033[1;32mPASSED\033[0m"
 echo "::group::Response from nginx"
 cat /tmp/last
 echo "::endgroup::" 
-[ "${http_code}" == "404" ] || exit 6
+[ "${http_code}" == "200" ] || exit 6
 
 echo -en " Test 7: \033[1mA wrong user with a valid standard token (6 digits / 30sec) is reject\033[0m... "
 http_code=$( curl -u:dummy:$( python tests/getOTP.py ${SECRET} ) -sio /tmp/last -w "%{http_code}" http://localhost:8080/priv6d30s/index.html )
